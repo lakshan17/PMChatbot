c987d8b68d55b2546e929037df059f8d2f105f68
Lakshan Pathum <107621003+lakshan17@users.noreply.github.com>
2022-08-07 17:28:02 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -18,7 +18,7 @@ async def force_sub(bot, msg):
                 return 
         except UserNotParticipant:
             text = await msg.reply_text("**❌ Access Denied ❌**\n🌷You Must Join My Update Channel...🌷\n♻️Join it & Try Again.♻️"),
-                reply_markup=InlineKeyboardMarkup([[
+                 reply_markup=InlineKeyboardMarkup([[
                  InlineKeyboardButton('𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊', url='https://t.me/ImDark_Empire'),
                  InlineKeyboardButton('𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊', url='https://t.me/SL_BOTS_TM')
                  ]]
