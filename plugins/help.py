e70731e76f804dcacb3737edca89adc0cd7d94a7
Lakshan Pathum <107621003+lakshan17@users.noreply.github.com>
2022-08-07 17:15:08 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -17,7 +17,7 @@ async def force_sub(bot, msg):
                 await msg.reply_text("Yourt Banned")
                 return 
         except UserNotParticipant:
-            await msg.reply_text text= 
+            await msg.reply_text
                 text="**❌ Access Denied ❌**\n🌷You Must Join My Update Channel...🌷\n♻️Join it & Try Again.♻️",
                 reply_markup=InlineKeyboardMarkup([[
                  InlineKeyboardButton('𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊', url='https://t.me/ImDark_Empire'),
